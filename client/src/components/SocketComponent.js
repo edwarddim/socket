494636d6f6a18517fca6d634aad30aa8b50471d9
Edward <eddieim@gmail.com>
2020-03-12 18:24:24 -0700

    front-end client / basic handshake

--- client/src/components/SocketComponent.js
+++ client/src/components/SocketComponent.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import io from 'socket.io-client'
 
 const SocketComponent = () =>{
+
+    const [socket] = useState(io(":8000"))
+
+    useEffect(()=>{
+        socket.on("client_side", msg=>{
+            console.log(msg)
+        })
+    }, [])
+
     return(
         <h1>SOCKET</h1>
     )
--- server.js
+++ server.js
@@ -9,5 +9,5 @@ const server = app.listen(8000, ()=> console.log("EXPRESS ON 8000"))
 const io = require("socket.io")(server)
 
 io.on('connection', socket=>{
-    console.log(socket)
+    console.log(socket.id)
 })
