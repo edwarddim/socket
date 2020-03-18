505021bc6e0d04ab05ac5caa0c3ef913eaaae6d8
Edward <eddieim@gmail.com>
2020-03-18 09:49:10 -0700

    after lecture

--- client/src/components/SocketComponent.js
+++ client/src/components/SocketComponent.js
@@ -1,27 +1,53 @@
-import React, {useState, useEffect} from 'react'
+import React,{useState, useEffect} from 'react'
 import io from 'socket.io-client'
-
 const SocketComponent = () =>{
 
     const [socket] = useState(io(":8000"))
+    const [state, setState] = useState('')
+    const [chat, setChat] = useState([])
+    const [name, setName] = useState("")
 
-    useEffect(()=>{
-        socket.on("client_side", msg=>{
-            console.log(msg)
+    useEffect(() => {
+        socket.on("chat_history", data =>{
+            setChat(data)
         })
-        socket.on("new_user", msg =>{
-            console.log(msg)
+        socket.on("new_message", msg=>{
+            setChat(chat =>{
+                return [msg, ...chat]
+            })
         })
+
+        var promptName = prompt("What is your name??")
+        setName(promptName)
+
+
     }, [])
 
-    const clickHandler = ()=>{
-        socket.emit("click", "Edward Im")
+    const submitHandler = (e) =>{
+        e.preventDefault()
+        // setChat(() =>{
+        //     return [state, ...chat]
+        // })
+        socket.emit("send_message" , {
+            name:name,
+            msg:state
+        })
     }
 
     return(
         <div>
             <h1>SOCKET</h1>
-            <button onClick={clickHandler}>Click me!</button>
+            <form onSubmit={submitHandler}>
+                <input onChange={(e) => setState(e.target.value)} />
+                <button type="submit">Send</button>
+            </form>
+            {chat.map((item, i) => (
+                <div key={i}>
+                    <p>{item.name} says:</p>
+                    <p>{item.msg}</p>
+                    <hr></hr>
+                </div>
+            ))}
         </div>
     )
 }
--- server.js
+++ server.js
@@ -6,10 +6,19 @@ const app = express()
 
 
 const server = app.listen(8000, ()=> console.log("EXPRESS ON 8000"))
-const io = require("socket.io")(server)
+const io = require('socket.io')(server)
 
-io.on('connection', socket=>{
-    socket.broadcast.emit("new_user", "a new user has joined")
-    // socket.emit("client_side", "hello!!")
-    socket.on("click", data => console.log("Clicked!! ", data))
+const chat = []
+
+io.on('connection', socket =>{
+    socket.emit("chat_history", chat)
+
+    socket.on("send_message", msg => {
+        // console.log("The message from the client: ",msg)
+        chat.push(msg)
+        console.log(chat)
+        // socket.broadcast.emit("new_message", msg)
+        // socket.emit => to everyone but the sender
+        io.emit("new_message", msg)
+    })
 })
